3cee3cc05b5654336f76f83899a54210f6b4e812
Lilli4nW <156350939+Lilli4nW@users.noreply.github.com>
2025-05-01 12:29:11 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 'use strict'
 
+//Trying desperately to toggle light dark mode, but it didn't work.
 let darkMode = document.getElementById("darkButton");
 let dark = document.querySelector("turnDark")
-
-
 darkMode.addEventListener("click", function(){
     darkMode.classList.toggle("darkButton");
 });
+
+
 
