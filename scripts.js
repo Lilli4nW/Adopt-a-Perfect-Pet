0b2a964e3c124895e6ef3ae2f733263500e1875d
Lilli4nW <156350939+Lilli4nW@users.noreply.github.com>
2025-05-01 12:58:14 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 'use strict'
 
-//Trying desperately to toggle light dark mode, but it didn't work.
+//Trying desperately to toggle light dark mode, but it didn't work the way I want it to.
 let darkMode = document.getElementById("darkButton");
 let dark = document.querySelector("turnDark")
 darkMode.addEventListener("click", function(){
     darkMode.classList.toggle("darkButton");
 });
 
-
-
+function yourEmail(){
+    let emailInput = document.getElementById("email");
+    
+}
